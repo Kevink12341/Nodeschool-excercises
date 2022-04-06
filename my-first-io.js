080c69266a9a3f7d80d0918c00602867b15f4709
Kevink12341 <77891062+Kevink12341@users.noreply.github.com>
2022-04-06 11:37:39 +0200

    Delete my-first-io.js

--- my-first-io.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const Buffer = require('buffer');
-
-let data = fs.readFileSync(process.argv[2]);
-const str = Buffer.Buffer.from(data,'hex');
-const count = str.toString('utf8')
-const numbers = count.split('\n')
-console.log(numbers.length-1)
